2026c2483ff0195449d930553ce32f3c4d912036
CrazyPunky <leanbaierpersonal@outlook.com>
2023-01-20 18:57:24 -0300

    Initial Proyect

---
+++ Spring/prices/src/test/java/com/matriz/lb/prices/PricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matriz.lb.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Spring/products/src/test/java/com/matriz/lb/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matriz.lb.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
