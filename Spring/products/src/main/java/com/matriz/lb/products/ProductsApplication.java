fa8c5a552f5777acbdad4b0fa89df6531e89078c
CrazyPunky <leanbaierpersonal@outlook.com>
2023-01-20 22:37:02 -0300

    Servicios y config server

--- Spring/products/src/main/java/com/matriz/lb/products/ProductsApplication.java
+++ Spring/products/src/main/java/com/matriz/lb/products/ProductsApplication.java
@@ -1,5 +1,6 @@
 package com.matriz.lb.products;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
